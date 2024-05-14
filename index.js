258db86824a52f87a5e51df4f1b6c5bdc9c91034
Teaghan <126303770+TeaghanEveleigh@users.noreply.github.com>
2024-05-14 16:43:03 +1200

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ const maxAge = 10 * 1000;
 
 // Setup express app
 const app = express();
-app.use(cors({
-  origin: 'https://teaghaneveleigh.github.io'
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
   
 app.use(bodyParser.json());
 pool.connect((err) => {
