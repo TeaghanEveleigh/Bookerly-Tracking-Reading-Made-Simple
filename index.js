daa23d7fca67ac680c5a24b202c77aa5a1060b19
Teaghan <eteaghan@gmail.com>
2024-05-16 15:40:30 +1200

    added logout route

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ const maxAge = 10 * 1000;
 const allowedOrigins = ['http://localhost:3000', 'https://teaghaneveleigh.github.io'];
 // Setup express app
 const app = express();
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins.includes(origin) ? origin : '');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,  // Important for sending cookies
+}));
+
 app.use(session({
     secret: 'secret-code',
     resave: false,
