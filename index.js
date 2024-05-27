0b5247809da55b63a2c3376883a1cc548cce7f18
Teaghan <eteaghan@gmail.com>
2024-05-27 15:30:46 +1200

    async handler added

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ pool.connect((err) => {
 });
 
 function isAuthenticated(req, res, next) {
-    const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.headers['Authorization']?.split(' ')[1];
     console.log("token on backend is" , token)
     if(token) { 
         jwt.verify(token , 'my key' , (err, decoded ) => { // Use jwt.verify
